1a9838c1eee65825eb3bcc025a300eab0dfe234e
Muhammad Usman <musman932@hotmail.com>
2026-04-26 00:57:05 -0600

    Trying to understand how the routes will work for the login page

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,13 +1,12 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import loginPage from "./routes/authRoutes"
 
 const app = express();
 const PORT = 4000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({ message: "API is running" });
-});
+app.get("/", loginPage);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
