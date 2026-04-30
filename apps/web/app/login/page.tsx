2149683c6044f69702c8ec0b647d4c09c4f35562
Muhammad Usman <musman932@hotmail.com>
2026-04-29 22:31:43 -0600

    Improved the login page and is almost fully complete

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -2,9 +2,16 @@ import LoginForm from '@/components/auth/LoginForm'
 
 export default function LoginPage() {
     return (
-        <div className="min-h-screen flex flex-col justify-center items-center">
-            <div className="px-25 pb-50 pt-5 flex flex-col items-center justify-center gap-4 bg-purple-200 rounded-lg">
-                <h1 className="text-black font-bold mb-auto">Login Page</h1>
+        <div className="min-h-screen flex flex-col justify-center items-center bg-[url('/images/login-background.jpg')] bg-cover bg-center">
+            <div className="w-full max-w-md px-8 pb-12 pt-5 flex flex-col items-center justify-center gap-4 rounded-2xl drop-shadow-xl/50 bg-linear-to-b from-sky-200 to-white">
+                
+                <div className="flex flex-col justify-center items-center bg-sky-100 shadow-xl/20 rounded-lg size-12">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                        <path fillRule="evenodd" d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6Zm-5.03 4.72a.75.75 0 0 0 0 1.06l1.72 1.72H2.25a.75.75 0 0 0 0 1.5h10.94l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 0 0-1.06 0Z" clipRule="evenodd" />
+                    </svg>
+                </div>
+
+                <h1 className="text-black font-bold">Sign in with email</h1>
                 <LoginForm></LoginForm>
             </div>
         </div>
--- apps/web/components/auth/LoginForm.tsx
+++ apps/web/components/auth/LoginForm.tsx
@@ -1,16 +1,38 @@
+'use client';
+
 import Form from 'next/form'
+import { RedirectType, redirect } from 'next/navigation';
+import React from 'react';
 
 export default function LoginForm() {
     async function loginUser(formData: FormData) {
         'use server';
         console.log("loginUser function called");
-    }
+    };
+
+    function handleSignUpButtonClick(event: React.MouseEvent<HTMLButtonElement>) {
+        redirect('/signup', RedirectType.replace);
+    };
 
     return (
-        <Form action={loginUser} className='flex flex-col gap-y-5'>
-            <input name="Email:" className='bg-gray-100 outline-3 outline-red-500'/>
-            <input name="Password:" className='bg-gray-100 outline-3 outline-red-500'/>
-            <button type="submit" className="text-white font-bold py-2 mx-7 rounded-full bg-sky-500 cursor-pointer hover:bg-sky-700">Login</button>
-        </Form>
+        <div className="flex flex-col gap-y-6">
+            <Form action={loginUser} className='flex flex-col gap-y-5'>
+                <input placeholder='Email'
+                className='w-full px-4 py-2 bg-gray-100 rounded-md shadow-lg/30'
+                />
+                <input placeholder='Password'
+                className='w-full px-4 py-2 bg-gray-100 rounded-md shadow-lg/30'
+                />
+                <button type="submit" className="mt-2 w-full text-white font-bold py-2 rounded-full bg-sky-500 cursor-pointer hover:bg-sky-700">Login</button>
+            </Form>
+            <div className="flex items-center gap-4">
+                <div className="flex-1 border-t-2 border-dotted border-gray-300"></div>
+                <span className="text-gray-500 text-sm">Or Sign Up</span>
+                <div className="flex-1 border-t-2 border-dotted border-gray-300"></div>
+            </div>
+            <button type="button" onClick={handleSignUpButtonClick}>
+                Sign Up
+            </button>
+        </div>
     );
 }
